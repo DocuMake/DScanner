37e20d4b8a11e9958c09324a2fd05423a19d57a9
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-10-24 15:06:42 -0500

    Create Logger.kt

---
+++ src/commonMain/kotlin/xyz/calcugames/documake/dscanner/parser/Logger.kt
@@ -0,0 +1,8 @@
+package xyz.calcugames.documake.dscanner.parser
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+/**
+ * The DScanner logger instance.
+ */
+val logger = KotlinLogging.logger("DScanner")
